b4bca0ef94d855783f52f22863398d863712024b
orionheidi <hajdidjukic@gmail.com>
2019-03-18 13:17:01 +0100

    6 filter products

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
+    <input type = 'text' v-model='search' placeholder="search products" />
      <table style="width:100%">
     <tr>
       <th>Id</th>
       <th>Name</th>
-      </tr>
-    <tr v-for="(product,index) in products" :key="index"> 
+    </tr>
+    <tr v-for="(product,index) in filteredProducts" :key="index"> 
       <td> {{product.id}}</td>
       <td> {{product.name}} </td>
     </tr>
@@ -15,9 +16,22 @@
 
 <script>
 export default {
+    data() {
+    return {
+        search: ''
+    }
+  },
       props: {
       products: {
           type: Array
+      }
+  },
+
+  computed:{
+      filteredProducts (){
+          return this.products.filter((product)=>{
+              return product.name.match(this.search)
+          });
       }
   },
       created (){
